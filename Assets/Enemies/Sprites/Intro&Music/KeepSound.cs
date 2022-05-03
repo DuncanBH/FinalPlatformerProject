ea55445c306507ef1de6a393dbb4cdcd3421ddce
dufourmax1738 <77691530+dufourmax1738@users.noreply.github.com>
2022-05-03 19:07:40 -0400

    made song not restart with the level

---
+++ Assets/Enemies/Sprites/Intro&Music/KeepSound.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeepSound : MonoBehaviour
+{
+    void Awake()
+    {
+        GameObject[] objs = GameObject.FindGameObjectsWithTag("Music");
+
+        if (objs.Length > 1)
+        {
+            Destroy(this.gameObject);
+        }
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
